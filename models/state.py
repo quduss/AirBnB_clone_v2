3175d769862da8b7962d740a81e5964ac581cd60
quduss <shuaibqudus33@gmail.com>
2024-02-06 12:30:06 +0100

    Map class to states table and create one to many relationship with cities table

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models import storage
+import shlex
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        objects = storage.all()
+        all_cities = []
+        my_cities = []
+        for key in objects:
+            obj_key = key.replace('.', ' ')
+            obj_key = shlex.split(obj_key)
+            if (obj_key[0] == 'City'):
+                all_cities.append(objects[key])
+        for city in all_cities:
+            if (city.state_id == self.id):
+                my_cities.append(city)
+        return (my_cities)
