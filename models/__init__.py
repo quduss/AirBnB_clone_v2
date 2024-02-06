471957b66492d5ed31edd1ec2f0504cf4920aa7d
quduss <shuaibqudus33@gmail.com>
2024-02-06 12:31:38 +0100

    Allow switch between filestorage and DBStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,9 +9,13 @@ from models.review import Review
 from models.place import Place
 from models.amenity import Amenity
 from models.city import City
+from os import getenv
 
 
-storage = FileStorage()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
 
 classes = {
