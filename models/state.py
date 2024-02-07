7790c599bb02ea91c0581a463c06a01e378036fe
quduss <shuaibqudus33@gmail.com>
2024-02-07 15:35:26 -0800

    Use if/else to distinguish between dbstorage and filestorage

--- models/state.py
+++ models/state.py
@@ -12,20 +12,22 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                              backref="state")
 
-    @property
-    def cities(self):
-        objects = models.storage.all()
-        all_cities = []
-        my_cities = []
-        for key in objects:
-            obj_key = key.replace('.', ' ')
-            obj_key = shlex.split(obj_key)
-            if (obj_key[0] == 'City'):
-                all_cities.append(objects[key])
-        for city in all_cities:
-            if (city.state_id == self.id):
-                my_cities.append(city)
-        return (my_cities)
+    else:
+        @property
+        def cities(self):
+            objects = models.storage.all()
+            all_cities = []
+            my_cities = []
+            for key in objects:
+                obj_key = key.replace('.', ' ')
+                obj_key = shlex.split(obj_key)
+                if (obj_key[0] == 'City'):
+                    all_cities.append(objects[key])
+            for city in all_cities:
+                if (city.state_id == self.id):
+                    my_cities.append(city)
+            return (my_cities)
