602848130c1f1207b9dc54fa0615ecffcd5c6b14
quduss <shuaibqudus33@gmail.com>
2024-08-03 20:12:18 +0100

    Load state, city and amenity

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    The States are sorted by name.
+    """
+    states = storage.all(State)
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state(id):
+    """Displays an HTML page that lists the state with the given id
+    . It also lists the cities in that state in a sorted manner. If
+    the id provided doesn't link to a valid state, a 'Not Found'
+    message is displayed"""
+    key = f'State.{id}'
+    if key in storage.all():
+        obj = storage.all()[key]
+    else:
+        obj = None
+    return render_template("9-states.html", obj=obj)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def filters():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+@app.teardown_appcontext
+def teardown(exception=None):
+    """Removes the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
